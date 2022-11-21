15732610171607d410a0b3ea9febd6ab539d9deb
Forrest Almasi <_@forrestalmasi.com>
2022-11-21 17:50:08 -0500

    move handler to controller

--- src/controllers/shirts.go
+++ src/controllers/shirts.go
@@ -1,14 +1,49 @@
 package controllers
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
-func ShirtPostHandler(c echo.Context) error {
-	// name := c.FormValue("name")
-	// email := c.FormValue("email")
+type PostShirtSuccess struct {
+	Message string `json:"message" xml:"message"`
+	Url     string `json:"url" xml:"url"`
+}
 
-	return c.JSON(http.StatusCreated, "ShirtCreated")
+func ShirtPostHandler(c echo.Context) error {
+
+	image, err := c.FormFile("image")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	// Source
+	src, err := image.Open()
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create("./images/" + image.Filename)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	m := &PostShirtSuccess{
+		Message: "Success",
+		Url:     image.Filename,
+	}
+
+	return c.JSONPretty(http.StatusCreated, m, "  ")
+
 }
--- src/index.go
+++ src/index.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	// "bytes"
-	"io"
 	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/malls/thankyou-api/src/controllers"
 )
-
-type PostShirtSuccess struct {
-	Message string `json:"message" xml:"message"`
-	Url     string `json:"url" xml:"url"`
-}
 
 func main() {
 	e := echo.New()
@@ -22,40 +15,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.POST("/shirt", func(c echo.Context) error {
-
-		image, err := c.FormFile("image")
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err)
-		}
-
-		// Source
-		src, err := image.Open()
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err)
-		}
-		defer src.Close()
-
-		// Destination
-		dst, err := os.Create("./images/" + image.Filename)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err)
-		}
-		defer dst.Close()
-
-		// Copy
-		if _, err = io.Copy(dst, src); err != nil {
-			return c.JSON(http.StatusBadRequest, err)
-		}
-
-		m := &PostShirtSuccess{
-			Message: "Success",
-			Url:     image.Filename,
-		}
-
-		return c.JSONPretty(http.StatusCreated, m, "  ")
-	})
-
+	e.POST("/shirt", controllers.ShirtPostHandler)
 	e.GET("/status", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Running Okay")
 	})
